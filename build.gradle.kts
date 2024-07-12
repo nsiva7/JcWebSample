88c4ec27632586fcc93c8b136824e4628dee5b4b
Siva Nimmala <sivanimmala.dev@gmail.com>
2024-07-13 00:14:02 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,25 @@
 plugins {
-    kotlin("multiplatform") version "1.8.0"
+    kotlin("js") version "1.7.0"
+}
+
+repositories {
+    mavenCentral()
 }
 
 kotlin {
-    js(IR) {
+    js {
         browser {
-            commonWebpackConfig {
-                cssSupport {
-                    enabled = true
-                }
+            webpackTask {
+                output.libraryTarget = "umd"
             }
         }
         binaries.executable()
     }
-    sourceSets {
-        val jsMain by getting {
-            dependencies {
-                implementation("org.jetbrains.compose.web:web:1.0.0-alpha4-build340")
-            }
-        }
+}
+
+tasks {
+    register<Copy>("copyDistributions") {
+        from("build/distributions")
+        into("docs")
     }
 }
